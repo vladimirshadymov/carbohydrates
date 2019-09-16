fca36c02870cb01cbf485400276da0a76feb0c63
vladimirshadymov <vladimir.shadymov@gmail.com>
2019-09-17 01:07:16 +0300

    Add hybridization detecting function.

--- utils.py
+++ utils.py
@@ -25,3 +25,28 @@ def get_bond_graph(atoms, bond_length=1.85):
 def get_atom_idxs(atoms, atom_type):
     return np.nonzero(atoms.get_atomic_numbers() == atom_type)
 
+
+def calc_carb_hybr(atoms):
+    bond_graph = get_bond_graph(atoms)
+    carbon_idxs = get_atom_idxs(atoms, atom_type=6)
+    print(type(carbon_idxs))
+    sp1 = []
+    sp2 = []
+    sp3 = []
+    others = []
+    for carbon in carbon_idxs:
+        if len(list(bond_graph.neighbors(carbon))) == 2:
+            sp1.append(carbon)
+        elif len(list(bond_graph.neighbors(carbon))) == 3:
+            sp2.append(carbon)
+        elif len(list(bond_graph.neighbors(carbon))) == 4:
+            sp3.append(carbon)
+        else:
+            others.append(carbon)
+
+    sp1 = np.array(sp1)
+    sp2 = np.array(sp2)
+    sp3 = np.array(sp3)
+    others = np.array(others)
+
+    return (sp1, sp2, sp3, others)
